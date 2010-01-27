Joe-xXx/gdal

commit 0afd83d33562ce15df1744484459599616969dc6

Make gdal_merge into a module. Add a global version variable. (#3161)
git-svn-id: https://svn.osgeo.org/gdal/trunk@18412 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/swig/python/scripts/gdal_merge.py
@@ -34,6 +34,7 @@ except ImportError:
 import sys
 import glob
 
+__version__ = '$id$'[5:-1]
 verbose = 0
 quiet = 0
 
@@ -244,7 +245,7 @@ def Usage():
 # Program mainline.
 #
 
-if __name__ == '__main__':
+def main( argv=None ):
 
     names = []
     format = 'GTiff'
@@ -261,7 +262,9 @@ if __name__ == '__main__':
     createonly = 0
     
     gdal.AllRegister()
-    argv = gdal.GeneralCmdLineProcessor( sys.argv )
+    if argv is None:
+        argv = sys.argv
+    argv = gdal.GeneralCmdLineProcessor( argv )
     if argv is None:
         sys.exit( 0 )
 
@@ -455,3 +458,6 @@ if __name__ == '__main__':
     
     # Force file to be closed.
     t_fh = None
+
+if __name__ == '__main__':
+    sys.exit(main())

commit 620985d874fd419b1d524f0ad01771f6a2b21574

Fixed the global variable issue caused by r18412 (#3354). Also adds support to specify seperate value for each band in gdal_merge.py
git-svn-id: https://svn.osgeo.org/gdal/trunk@18668 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/gdal_utilities.dox
@@ -1071,7 +1071,7 @@ mosaics a set of images
 \verbatim
 gdal_merge.py [-o out_filename] [-of out_format] [-co NAME=VALUE]*
                      [-ps pixelsize_x pixelsize_y] [-separate] [-v] [-pct]
-                     [-ul_lr ulx uly lrx lry] [-n nodata_value] [-init value]
+                     [-ul_lr ulx uly lrx lry] [-n nodata_value] [-init "value [value...]"]
                      [-ot datatype] [-createonly] input_files
 \endverbatim
 
@@ -1114,8 +1114,9 @@ color table.
 Ignore pixels from files being merged in with this pixel value. 
 </dd>
 <dt> <b>-init</b> <i>value</i>:</dt><dd> 
-Pre-initialize the output file with this value.  However, it is not marked
-as the nodata value in the output file. 
+Pre-initialize the output image bands with these values.  However, it is not
+marked as the nodata value in the output file.  If only one value is given, the
+same value is used in all the bands. 
 </dd>
 <dt> <b>-createonly</b>:</dt><dd> 
 The output file is created (and potentially pre-initialized) but no input

gdal/swig/python/scripts/gdal_merge.py
@@ -235,7 +235,7 @@ class file_info:
 def Usage():
     print('Usage: gdal_merge.py [-o out_filename] [-of out_format] [-co NAME=VALUE]*')
     print('                     [-ps pixelsize_x pixelsize_y] [-separate] [-q] [-v] [-pct]')
-    print('                     [-ul_lr ulx uly lrx lry] [-n nodata_value] [-init value]')
+    print('                     [-ul_lr ulx uly lrx lry] [-n nodata_value] [-init "value [value...]"]')
     print('                     [-ot datatype] [-createonly] input_files')
     print('                     [--help-general]')
     print('')
@@ -247,6 +247,9 @@ def Usage():
 
 def main( argv=None ):
 
+    global verbose, quiet
+    verbose = 0
+    quiet = 0
     names = []
     format = 'GTiff'
     out_file = 'out.tif'
@@ -257,7 +260,7 @@ def main( argv=None ):
     copy_pct = 0
     nodata = None
     create_options = []
-    pre_init = None
+    pre_init = []
     band_type = None
     createonly = 0
     
@@ -304,7 +307,9 @@ def main( argv=None ):
 
         elif arg == '-init':
             i = i + 1
-            pre_init = float(argv[i])
+            str_pre_init = argv[i].split()
+            for x in str_pre_init:
+                pre_init.append(float(x))
 
         elif arg == '-n':
             i = i + 1
@@ -424,8 +429,12 @@ def main( argv=None ):
 
     # Do we need to pre-initialize the whole mosaic file to some value?
     if pre_init is not None:
-        for i in range(t_fh.RasterCount):
-            t_fh.GetRasterBand(i+1).Fill( pre_init )
+        if t_fh.RasterCount <= len(pre_init):
+            for i in range(t_fh.RasterCount):
+                t_fh.GetRasterBand(i+1).Fill( pre_init[i] )
+        elif len(pre_init) == 1:
+            for i in range(t_fh.RasterCount):
+                t_fh.GetRasterBand(i+1).Fill( pre_init[0] )
 
     # Copy data from source files into output file.
     t_band = 1